3f985ed582d533eaba65fda652f006902f3b0a07
Andrey <an_manaev1988@mail.ru>
2022-11-09 14:56:37 +0400

    add format-mode

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -3,11 +3,11 @@ This Cli-program return differents two files
 """
 
 from gendiff.engine import generate_diff
-from gendiff.cli import path1, path2
+from gendiff.cli import path1, path2, format_name
 
 
 def main():
-    print(generate_diff(path1, path2))
+    print(generate_diff(path1, path2, format_name))
 
 
 if __name__ == '__main__':
